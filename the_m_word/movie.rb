01e5858e35ada5c1bfd5a95f71877396fa6d5d0e
Christopher Kramlich <chris@kramlich.com>
2015-07-22 17:49:30 -0700

    Adding refactored movie.rb

--- the_m_word/movie.rb
+++ the_m_word/movie.rb
@@ -1,21 +1,11 @@
-class Movie < Entity
-  def intialize(ident)
-    super "movies", ident
-  end
+require 'active_record'
 
-  def title
-    get "title"
-  end
-
-  def title=(value)
-    set "title", value
-  end
-
-  def director
-    get "director"
-  end
-
-  def director=(value)
-    set "director", value
-  end
+class Movie < ActiveRecord::Base
 end
+=begin
+
+movie = Movie.create
+movie.title = "Doctor Strangelove"
+movie.title # => "Doctor Strangelove"
+
+=end
